fb68da8e681873de7a84ee1ec1179c91c810ce8a
yashar-akish <yashar_akish@yahoo.com>
2021-05-16 12:06:38 -0400

    prepare fragment object in testFragmentActivity

--- app/src/main/java/com/company/sendingdata/TestFragmentActivity.java
+++ app/src/main/java/com/company/sendingdata/TestFragmentActivity.java
@@ -1,14 +1,26 @@
 package com.company.sendingdata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TestFragmentActivity extends AppCompatActivity {
+
+    TextView tv_f_name, tv_f_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        TestFragment testFragment = new TestFragment();
+        fragmentTransaction.add(R.id.linear, testFragment);
+        fragmentTransaction.commit();
+
     }
 }
