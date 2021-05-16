075125d99c257f4796fa4227cb3aa08ece4de215
yashar-akish <yashar_akish@yahoo.com>
2021-05-16 15:04:58 -0400

    preparing firstFragment in twoFragmentActivity

--- app/src/main/java/com/company/sendingdata/TwoFragmentsActivity.java
+++ app/src/main/java/com/company/sendingdata/TwoFragmentsActivity.java
@@ -1,6 +1,8 @@
 package com.company.sendingdata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class TwoFragmentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_fragments);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FirstFragment firstFragment = new FirstFragment();
+        fragmentTransaction.replace(R.id.frameLayout, firstFragment);
+        fragmentTransaction.commit();
     }
 }
