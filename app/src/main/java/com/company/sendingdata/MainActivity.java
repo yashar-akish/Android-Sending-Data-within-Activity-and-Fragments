53987691eb517ddf935d3c2082a7b5d596388ac0
yashar-akish <yashar_akish@yahoo.com>
2021-05-15 23:30:00 -0400

    creating Intent with 'put extra' from mainActivity to secondActivity

--- app/src/main/java/com/company/sendingdata/MainActivity.java
+++ app/src/main/java/com/company/sendingdata/MainActivity.java
@@ -2,13 +2,40 @@ package com.company.sendingdata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_name, et_email, et_phone;
+    Button btn_signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_name = findViewById(R.id.et_name);
+        et_email = findViewById(R.id.et_email);
+        et_phone = findViewById(R.id.et_phone);
+        btn_signUp = findViewById(R.id.btn_signUp);
+
+        btn_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = et_name.getText().toString();
+                String userEmail = et_email.getText().toString();
+                int userPhone = Integer.parseInt(et_phone.getText().toString());
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", userName);
+                intent.putExtra("email", userEmail);
+                intent.putExtra("phone", userPhone);
+                startActivity(intent);
+            }
+        });
     }
 }
