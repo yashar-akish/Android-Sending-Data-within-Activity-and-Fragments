26c628b6ef8a1598fa6c0e6b8081f8ad092a0804
yashar-akish <yashar_akish@yahoo.com>
2021-05-16 00:41:00 -0400

    creating and designing fragment

---
+++ app/src/main/java/com/company/sendingdata/bmiFragment.java
@@ -0,0 +1,24 @@
+package com.company.sendingdata;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class bmiFragment extends Fragment {
+
+
+    public bmiFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bmi, container, false);
+    }
+}
