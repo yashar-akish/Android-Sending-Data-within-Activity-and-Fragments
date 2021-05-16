249b2e1d46ec162c80e55335b625a0c58bebc4a5
yashar-akish <yashar_akish@yahoo.com>
2021-05-16 02:03:38 -0400

    adding codes to bmiFragment

--- app/src/main/java/com/company/sendingdata/BmiFragment.java
+++ app/src/main/java/com/company/sendingdata/BmiFragment.java
@@ -7,9 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class BmiFragment extends Fragment {
+
+    TextView tv_result;
 
 
     public BmiFragment() {
@@ -19,6 +22,18 @@ public class BmiFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bmi, container, false);
+        View view = inflater.inflate(R.layout.fragment_bmi, container, false);
+
+        tv_result = view.findViewById(R.id.tv_result);
+
+        Bundle bundle = getArguments();
+        int userWeight = bundle.getInt("weight");
+        int userHeight = bundle.getInt("height");
+
+        double userBMI = (userWeight * 1000) / (userHeight * userHeight);
+
+        tv_result.setText("Your BMI is: "+userBMI);
+
+        return view;
     }
 }
