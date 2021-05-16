7813544d6dad06c7a5ae77cac5dcd5a406e2dcfd
yashar-akish <yashar_akish@yahoo.com>
2021-05-16 15:18:19 -0400

    adding codes af firstFragment and preparing secondFragment with bundle

--- app/src/main/java/com/company/sendingdata/FirstFragment.java
+++ app/src/main/java/com/company/sendingdata/FirstFragment.java
@@ -1,25 +1,51 @@
 package com.company.sendingdata;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FirstFragment extends Fragment {
 
-
+    EditText et_f_f_name;
+    Button btn_f_f_send;
 
     public FirstFragment() {
         // Required empty public constructor
     }
-    
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        et_f_f_name = view.findViewById(R.id.et_f_f_name);
+        btn_f_f_send = view.findViewById(R.id.btn_f_f_send);
+
+        btn_f_f_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = et_f_f_name.getText().toString();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", userName);
+
+                //  creating second fragment:
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                SecondFragment secondFragment = new SecondFragment();
+                secondFragment.setArguments(bundle);
+
+                fragmentTransaction.replace(R.id.frameLayout, secondFragment);
+                fragmentTransaction.commit();
+
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/company/sendingdata/TwoFragmentsActivity.java
+++ app/src/main/java/com/company/sendingdata/TwoFragmentsActivity.java
@@ -1,10 +1,10 @@
 package com.company.sendingdata;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Bundle;
 
 public class TwoFragmentsActivity extends AppCompatActivity {
 
