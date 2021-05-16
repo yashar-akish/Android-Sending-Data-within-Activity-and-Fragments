f03b5af972a7d1739d81f1406aa59cd66c032e26
yashar-akish <yashar_akish@yahoo.com>
2021-05-15 23:36:08 -0400

    design secondActivity

--- app/src/main/java/com/company/sendingdata/MainActivity.java
+++ app/src/main/java/com/company/sendingdata/MainActivity.java
@@ -1,12 +1,12 @@
 package com.company.sendingdata;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
